db5aba03de41a409579c8cec8d2f12f98ee5427f
Paul Ebose <paulebose@gmail.com>
2021-05-12 18:30:55 +0100

    add google's `go-cmp` to print out diffs

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/paulebose/uno
 
 go 1.16
+
+require github.com/google/go-cmp v0.5.5
--- uno.go
+++ uno.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 
+	"github.com/google/go-cmp/cmp"
+
 	"github.com/paulebose/uno/strextra"
 )
 
 func main() {
 	fmt.Println("Hello People!")
 	fmt.Println(strextra.ReverseRunes("Hello People!"))
+	fmt.Println(cmp.Diff("Hello Boy", "Hello People!"))
 }
