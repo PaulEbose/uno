9be3024e88528aa34df5a4f115337a5edbee8a57
Paul Ebose <paulebose@gmail.com>
2021-05-12 18:22:24 +0100

    add `strextra` package

--- uno.go
+++ uno.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/paulebose/uno/strextra"
+)
 
 func main() {
 	fmt.Println("Hello People!")
+	fmt.Println(strextra.ReverseRunes("Hello People!"))
 }
